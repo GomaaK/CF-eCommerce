dbc3e79b55ccd9a9163ca32faad682ccf387846e
tiago <tiago@shapeways.com>
2016-06-14 11:01:54 +0200

    add header image and sticky nav

--- MyFirstApp/app/assets/javascripts/application.js
+++ MyFirstApp/app/assets/javascripts/application.js
@@ -14,3 +14,24 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#sticky-anchor').offset().top;
+    if (window_top > div_top) {
+        $('#sticky').addClass('stick');
+        $('#sticky-anchor').height($('#sticky').outerHeight());
+    } else {
+        $('#sticky').removeClass('stick');
+        $('#sticky-anchor').height(0);
+   }
+}
+
+
+
+$(function() {
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+});
+
+
