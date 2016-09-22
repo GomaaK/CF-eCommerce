3d88189ba393aeea385d7a0b132633d45c4a0a7c
tiago <tiago@shapeways.com>
2016-09-22 22:37:28 +0200

    AJAX

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,6 +1,5 @@
-$(document).on('ready page:load', function(){
-	$('.img-zoom').elevateZoom();
-    $('.rating').raty({ path: '/assets/images', scoreName: 'comment[rating]'  }
+var refreshRating = function() {
+	$('.rating').raty({ path: '/assets/images', scoreName: 'comment[rating]'  }
     	);
     $('.rated').raty({ path: '/assets/images',
       readOnly: true,
@@ -8,6 +7,14 @@ $(document).on('ready page:load', function(){
         return $(this).attr('data-score');
       }
     });
+}
+
+
+
+
+$(document).on('ready page:load', function(){
+	$('.img-zoom').elevateZoom();
+    refreshRating();
 });
 
 
